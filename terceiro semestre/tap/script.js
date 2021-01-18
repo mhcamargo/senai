97c57d8d832439940080d73b8ed4f54571ca8736
Murilo Camargo <67740134+mhcamargo@users.noreply.github.com>
2021-01-18 09:02:41 -0200

    :maple_leaf: Tap completo

--- terceiro semestre/tap/script.js
+++ terceiro semestre/tap/script.js
@@ -1,21 +1,26 @@
-const descricao = document.getElementById("desc")
-const patrocinadores = document.getElementById("patroci")
-const rh = document.getElementById("rh")
-const gerentProj = document.getElementById("g-proj")
-const patrocinador = document.getElementById("patro")
-const client = document.getElementById("clien")
-const data = document.getElementById("dat-con")
-const btn = document.getElementById("btn")
+const descricao = document.getElementById("desc");
+const patrocinadores = document.getElementById("patroci");
+const rh = document.getElementById("rh");
+const gerentProj = document.getElementById("g-proj");
+const patrocinador = document.getElementById("patro");
+const client = document.getElementById("clien");
+const data = document.getElementById("dat-con");
+const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
-
-    var doc = new jsPDF()
-    doc.text(descricao.value, 10, 10)
-    doc.text(patrocinadores.value, 10, 20)
-    doc.text(rh.value, 10, 30)
-    doc.text(gerentProj.value, 10, 40)
-    doc.text(patrocinador.value, 10, 50)
-    doc.text(client.value, 10, 60)
-    doc.text(data.value, 10, 70)
-    doc.save('teste.pdf')
+    if (descricao.value !== '' && patrocinadores.value !== '' && rh.value !== '' && gerentProj.value !== '' && patrocinador.value !== '' && client.value !== '' && data.value !== '' && data.value !== '') {
+        var doc = new jsPDF();
+        doc.addImage('./princ.png', 'PNG', 25, 0, 165, 233);
+        doc.setFontSize(12);
+        doc.text(descricao.value, 50, 38);
+        doc.text(patrocinadores.value, 50, 49);
+        doc.text(rh.value, 83, 60);
+        doc.text(gerentProj.value, 83, 121);
+        doc.text(patrocinador.value, 83, 131);
+        doc.text(client.value, 83, 141);
+        doc.text(data.value, 83, 152);
+        doc.save('projeto.pdf');
+    } else {
+        alert("Preencha todos os campos obrigatorios.");
+    }
 })      
